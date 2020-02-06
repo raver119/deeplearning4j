da1931a1beff4da74b60a1be873d04cc73fbe1d4
raver119 <raver119@gmail.com>
2020-02-06 15:56:59 +0300

    pragma replaced
    Signed-off-by: raver119 <raver119@gmail.com>

--- libnd4j/include/ops/declarable/helpers/impl/knn_mindistance.cpp
+++ libnd4j/include/ops/declarable/helpers/impl/knn_mindistance.cpp
@@ -34,7 +34,7 @@ namespace nd4j {
                 T po = 2.f;
                 T o = 1.f;
 
-#pragma omp simd reduction(sumT:res)
+                PRAGMA_OMP_PARALLEL_REDUCTION(sumT:res)
                 for (auto e = 0; e < length; e++) {
                     T p = input[e];
                     T l = low[e];
