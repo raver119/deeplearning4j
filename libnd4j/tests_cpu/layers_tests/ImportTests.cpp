eb75b6a59cfa446f8fb4d8da47bbda0ad33f5cb9
Yves Quemener <yves@iv-devs.com>
2020-02-18 12:23:14 +0900

    Starting a test for importing .fb files (indirectly) from tensorflow

---
+++ libnd4j/tests_cpu/layers_tests/ImportTests.cpp
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright (c) 2015-2018 Skymind, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
+//
+// @author raver119@gmail.com
+//
+
+#include "testlayers.h"
+#include <flatbuffers/flatbuffers.h>
+#include <graph/generated/node_generated.h>
+#include <graph/generated/graph_generated.h>
+#include <graph/Node.h>
+#include <graph/Graph.h>
+#include <graph/GraphUtils.h>
+#include <NDArray.h>
+#include <ops/declarable/DeclarableOp.h>
+#include <ops/declarable/generic/parity_ops.cpp>
+
+using namespace nd4j;
+using namespace nd4j::graph;
+
+class ImportTests : public testing::Test {
+public:
+    /*
+    int cShape[] = {2, 2, 2, 2, 1, 0, 1, 99};
+    int fShape[] = {2, 2, 2, 1, 2, 0, 1, 102};
+     */
+    ImportTests() {
+        //Environment::getInstance()->setDebug(true);
+        //Environment::getInstance()->setVerbose(true);
+    }
+};
+
+TEST_F(ImportTests, SingleInput1) {
+        const char* modelFilename = "resources/lstm_mnist.fb";
+    auto timeStart = std::chrono::system_clock::now();
+    nd4j_printf("Importing file:", 0);
+    auto graph = GraphExecutioner::importFromFlatBuffers(modelFilename);
+    auto timeEnd = std::chrono::system_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::milliseconds> ((timeEnd - timeStart)).count();
+    nd4j_printf(" %d ms\n", dt);
+    nd4j_printf("Building graph\n", 0);
+    graph->buildGraph();
+
+    auto placeholders = graph->getPlaceholders();
+    auto variablespace = graph->getVariableSpace()->getVariables();
+    nd4j_printf("Placeholders:\n------------\n",0);
+    for(Variable* ph: *placeholders){
+        nd4j_printf("%s\n", ph->getName()->c_str());
+    }
+    nd4j_printf("Variables:\n------------\n",0);
+    for(Variable* var: variablespace){
+        nd4j_printf("%s\n", var->getName()->c_str());
+        if(var->hasNDArray()) {
+            timeStart = std::chrono::system_clock::now();
+            if(!var->getNDArray()->isS())
+            {
+                NDArray scalar = var->getNDArray()->sumNumber();
+                dt = std::chrono::duration_cast<std::chrono::milliseconds> ((std::chrono::system_clock::now() - timeStart)).count();
+                nd4j_printf("Sum=%f, %d ms\n", scalar.e<float>(0), dt);
+            }
+        }
+    }
+
+    ASSERT_TRUE(true);
+}
