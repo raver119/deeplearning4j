30f339a5a77311904fb42681cc54eaf2c4e9f46c
Fariz Rahman <farizrahman4u@gmail.com>
2020-02-21 00:20:06 +0400

    ..

--- deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/TFOpLayer.java
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/TFOpLayer.java
@@ -7,6 +7,8 @@ import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.primitives.Pair;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TFOpLayer extends AbstractLayer<org.deeplearning4j.nn.conf.layers.TFOpLayer> {
@@ -25,6 +27,24 @@ public class TFOpLayer extends AbstractLayer<org.deeplearning4j.nn.conf.layers.T
     @Override
     public Pair<Gradient, INDArray> backpropGradient(INDArray epsilon, LayerWorkspaceMgr workspaceMgr){
         throw new RuntimeException("Backprop through TFOplayer is not supported yet.");
+    }
+
+    private static String pbtxtFromMap(Map<String, Object> map){
+        String ret = "";
+        ret += "node{\n";
+        for (Map.Entry<String, Object> e: map.entrySet()){
+            if (e.getValue() instanceof String){
+                ret += e.getKey() + ": " + e.getValue() + '\n';
+            }
+            else if (e.getValue() instanceof List){
+                List listVal = (List)e.getValue();
+                for(Object item:listVal){
+                    Map subMap = new HashMap();
+                    subMap.put(e.getKey(), item);
+                    //TODO
+                }
+            }
+        }
     }
     @Override
     public INDArray activate(boolean training, LayerWorkspaceMgr workspaceMgr){
