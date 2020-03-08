f3c23ab051073340295a26618fa07c80953aec42
Fariz Rahman <farizrahman4u@gmail.com>
2020-03-09 02:07:33 +0400

    headers

--- deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/TFOpLayer.java
+++ deeplearning4j/deeplearning4j-nn/src/main/java/org/deeplearning4j/nn/layers/TFOpLayer.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Konduit K.K.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ******************************************************************************/
+
 package org.deeplearning4j.nn.layers;
 
 import lombok.extern.slf4j.Slf4j;
@@ -113,6 +129,9 @@ public class TFOpLayer extends AbstractLayer<org.deeplearning4j.nn.conf.layers.T
         }
         INDArray out = graphRunner.run(inputMap).values().toArray(new INDArray[0])[0];
         log.debug(out.toString());
+        if (out.rank() == 3){
+            out = out.permute(1, 0, 2); // TODO post-processing?
+        }
         return out;
     }
 
