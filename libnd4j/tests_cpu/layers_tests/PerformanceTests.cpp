2c99f6ccd451abd8833c288b2bc71b974f1372ab
Yves Quemener <yves@iv-devs.com>
2020-03-12 19:13:30 +0900

    Fixed lstm bench test

--- libnd4j/tests_cpu/layers_tests/PerformanceTests.cpp
+++ libnd4j/tests_cpu/layers_tests/PerformanceTests.cpp
@@ -52,6 +52,92 @@
 using namespace sd;
 using namespace sd::graph;
 
+#define WARMUP 1
+#define NUM_ITER 1
+
+
+template <typename T>
+static std::string lstmBenchmark() {
+    std::string output;
+    output += "lstm " + DataTypeUtils::asString(DataTypeUtils::fromT<T>());
+    BenchmarkHelper helper(WARMUP, NUM_ITER);
+
+    BoolParameters format("format");    //0=TNS=[seqLen,mb,size]; 1=NST=[mb,size,seqLen]
+    PredefinedParameters mb("mb", {1, 8});
+    int n = 128;
+
+    ParametersBatch batch({&format, &mb});
+    sd::ops::lstmBlock lstmBlock;
+    DeclarableBenchmark benchmark(lstmBlock, "lstm");
+
+    int seqLength = 8;
+
+    auto generator = PARAMETRIC_D() {
+        auto ctx = new Context(1);
+        int f = p.getIntParam("format");
+        int m = p.getIntParam("mb");
+
+        Nd4jLong l = 0;
+        ctx->setInputArray(0, NDArrayFactory::create_<Nd4jLong>(l), true);  //Max TS length (unused)
+
+
+        if (f == 0) {
+            //TNS format
+            ctx->setInputArray(1, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);     //x
+            ctx->setOutputArray(0, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //i
+            ctx->setOutputArray(1, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //c
+            ctx->setOutputArray(2, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //f
+            ctx->setOutputArray(3, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //o
+            ctx->setOutputArray(4, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //z
+            ctx->setOutputArray(5, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //h
+            ctx->setOutputArray(6, NDArrayFactory::create_<T>('c', {seqLength, m, n}), true);    //y
+        } else {
+            //NST format
+            ctx->setInputArray(1, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);     //x
+            ctx->setOutputArray(0, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //i
+            ctx->setOutputArray(1, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //c
+            ctx->setOutputArray(2, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //f
+            ctx->setOutputArray(3, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //o
+            ctx->setOutputArray(4, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //z
+            ctx->setOutputArray(5, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //h
+            ctx->setOutputArray(6, NDArrayFactory::create_<T>('f', {m, n, seqLength}), true);    //y
+        }
+
+        auto cLast = NDArrayFactory::create_<T>('c', {m, n});
+        auto yLast = NDArrayFactory::create_<T>('c', {m, n});
+        auto W = NDArrayFactory::create_<T>('c', {2 * n, 4 * n});
+        auto Wci = NDArrayFactory::create_<T>('c', {n});
+        auto Wcf = NDArrayFactory::create_<T>('c', {n});
+        auto Wco = NDArrayFactory::create_<T>('c', {n});
+        auto b = NDArrayFactory::create_<T>('c', {4 * n});
+
+        ctx->setInputArray(2, cLast, true);
+        ctx->setInputArray(3, yLast, true);
+        ctx->setInputArray(4, W, true);
+        ctx->setInputArray(5, Wci, true);
+        ctx->setInputArray(6, Wcf, true);
+        ctx->setInputArray(7, Wco, true);
+        ctx->setInputArray(8, b, true);
+
+        auto iargs = new Nd4jLong[2];
+        iargs[0] = 0;   //No peephole
+        iargs[1] = f;
+        ctx->setIArguments(iargs, 2);
+        delete[] iargs;
+
+        auto targs = new double[2];
+        targs[0] = 1.0; //forget bias
+        targs[1] = 0.0; //cell clipping value
+        ctx->setTArguments(targs, 2);
+        delete[] targs;
+        return ctx;
+    };
+
+    output += helper.runOperationSuit(&benchmark, generator, batch, "LSTMBlock");
+    return output;
+}
+
+
 class PerformanceTests : public testing::Test {
 public:
     int numIterations = 100;
@@ -68,7 +154,7 @@ TEST_F(PerformanceTests, benchmarksLight) {
 }
 
 TEST_F(PerformanceTests, benchmarksFull) {
-    BenchmarkSuit bench;
+    FullBenchmarkSuit bench;
     bench.runSuit();
 }
 
