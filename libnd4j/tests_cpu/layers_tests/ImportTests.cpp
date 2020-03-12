7c5502d22b35522ffd29e7701d905ada0b35288d
Yves Quemener <yves@iv-devs.com>
2020-03-12 14:32:55 +0900

    Added big lstm test

--- libnd4j/tests_cpu/layers_tests/ImportTests.cpp
+++ libnd4j/tests_cpu/layers_tests/ImportTests.cpp
@@ -101,6 +101,43 @@ TEST_F(ImportTests, LstmMnist) {
     //0.046829
 }
 
+TEST_F(ImportTests, LstmMnistBig) {
+        const char* modelFilename = "resources/lstm_mnist_big.fb";
+    auto timeStart = std::chrono::system_clock::now();
+//    nd4j_printf("Importing file:", 0);
+    auto graph = GraphExecutioner::importFromFlatBuffers(modelFilename);
+    auto timeEnd = std::chrono::system_clock::now();
+    auto dt = std::chrono::duration_cast<std::chrono::milliseconds> ((timeEnd - timeStart)).count();
+//    nd4j_printf(" %d ms\n", dt);
+//    nd4j_printf("Building graph\n", 0);
+    graph->buildGraph();
+
+    auto placeholders = graph->getPlaceholders();
+    auto variablespace = graph->getVariableSpace()->getVariables();
+    int height = 28;
+    int width = 768;
+    int batchsize = 128;
+
+    NDArray* inputArray = NDArrayFactory::create_<double>('c', {batchsize, height, width});
+    Variable* input = new Variable(inputArray, "input");
+    graph->getVariableSpace()->replaceVariable(input);
+
+    auto profile = GraphProfilingHelper::profile(graph, 10);
+    profile->printOut();
+    delete profile;
+
+    Nd4jStatus status = GraphExecutioner::execute(graph);
+    std::string outputLayerName = "output";
+    NDArray* result = graph->getVariableSpace()->getVariable(&outputLayerName)->getNDArray();
+    std::vector<double> rvec = result->getBufferAsVector<double>();
+    for(int i=0; i<10; i++)
+        nd4j_debug("(%d): %f\n", i, rvec[i]);
+    ASSERT_NEAR(rvec[0], 0.046829, 0.0001);
+
+    //timers->displayTimers();
+    //0.046829
+}
+
 TEST_F(ImportTests, ConcatPerfTest) {
     auto x0 = NDArrayFactory::create<double>('c', {1,28});
     auto x1 = NDArrayFactory::create<double>('c', {1,128});
@@ -111,4 +148,29 @@ TEST_F(ImportTests, ConcatPerfTest) {
     sd::ops::concat op;
 
     auto result = op.evaluate({&x0, &x1}, {}, {1});
+
+    //timers->displayTimers();
+
+    /*auto timeStart = std::chrono::system_clock::now();
+    timers[198] = std::chrono::high_resolution_clock::now();
+    int suma = 1;
+    int sumb = 1;
+    for(int i=0;i<100000;i++){
+        int c = suma;
+        suma = sumb +suma;
+        sumb = c;
+    }
+    printf("suma = %d\n", suma);
+    nd4j_printf("Timers: %p\n", (void*)(timers));
+    timers[199] = std::chrono::high_resolution_clock::now();
+    auto lines = sd::GlobalTimers::getInstance()->line_numbers;
+    for(int i=1;i<200;i++){
+        if(lines[i-1]==0) continue;
+        if(lines[i]==0) continue;
+        auto dt1 = std::chrono::duration_cast<std::chrono::nanoseconds> ((timers[i] - timers[i-1])).count();
+        auto dt2 = std::chrono::duration_cast<std::chrono::microseconds> ((timers[i] - timers[i-1])).count();
+        auto dt3 = std::chrono::duration_cast<std::chrono::milliseconds> ((timers[i] - timers[i-1])).count();
+        nd4j_printf("i=%d, line=%d: %ld ns %ld us %ld ms\n", i,  sd::GlobalTimers::getInstance()->line_numbers[i], dt1, dt2, dt3);
+    }*/
+
 }
