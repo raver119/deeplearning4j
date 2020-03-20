8815a58d1e37ea00c6f2a6e09c02e349ad8214f4
Fariz Rahman <farizrahman4u@gmail.com>
2020-03-20 05:14:19 +0400

    rem bad import

--- nd4j/nd4j-tensorflow/src/main/java/org/nd4j/tensorflow/conversion/graphrunner/GraphRunner.java
+++ nd4j/nd4j-tensorflow/src/main/java/org/nd4j/tensorflow/conversion/graphrunner/GraphRunner.java
@@ -18,7 +18,6 @@ package org.nd4j.tensorflow.conversion.graphrunner;
 
 import lombok.*;
 import org.apache.commons.io.FileUtils;
-import org.nd4j.TFGraphRunner;
 import org.nd4j.base.Preconditions;
 import org.nd4j.linalg.api.buffer.DataType;
 import org.nd4j.linalg.factory.Nd4j;
