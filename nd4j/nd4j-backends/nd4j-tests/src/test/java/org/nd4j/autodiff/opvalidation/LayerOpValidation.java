1a4b01a8fdddcbdfe4b9ee9937d24942bbac3aa9
Andrii Tuzhykov <andrewtuzhykov@gmail.com>
2020-04-09 17:25:14 +0300

    EmbeddingLookup and DepthwiseConv2dBp finished + tests added
    Signed-off-by: Andrii Tuzhykov <andrewtuzhykov@gmail.com>

--- nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/opvalidation/LayerOpValidation.java
+++ nd4j/nd4j-backends/nd4j-tests/src/test/java/org/nd4j/autodiff/opvalidation/LayerOpValidation.java
@@ -1478,13 +1478,13 @@ public class LayerOpValidation extends BaseOpValidation {
         int kernelWidth = 2;
         int strideHeight = 2;
         int strideWidth = 2;
-        int inChannels = 10;
-        int outChannels = 5;
+        int inChannels = 2;
+        int outChannels = 3;
         Nd4j.getRandom().setSeed(12345);
         SameDiff sd = SameDiff.create();
-        SDVariable in = sd.var("in", Nd4j.rand(bS, inChannels, 28,28));
-        SDVariable weights = sd.var("weights", Nd4j.rand(DataType.FLOAT, kernelHeight, kernelWidth, inChannels, outChannels));
-        SDVariable bias = sd.var("bias", Nd4j.rand(DataType.FLOAT, 50));
+        SDVariable in = sd.var("in", Nd4j.rand(bS, inChannels, 5,5));
+        SDVariable weights = sd.var("weights", Nd4j.rand(DataType.DOUBLE, kernelHeight, kernelWidth, inChannels, outChannels));
+        SDVariable bias = sd.var("bias", Nd4j.rand(DataType.DOUBLE, inChannels*outChannels));
         Conv2DConfig config = Conv2DConfig.builder()
                 .kH(kernelHeight)
                 .kW(kernelWidth)
@@ -1502,7 +1502,5 @@ public class LayerOpValidation extends BaseOpValidation {
         );
 
 
-
     }
-
 }
