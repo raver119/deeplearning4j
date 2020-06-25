8b6aa54be837eb207afaa35bb5bb535116ced428
raver119@gmail.com <raver119@gmail.com>
2020-06-25 18:26:42 +0300

    2 more tests for Switch and While
    Signed-off-by: raver119@gmail.com <raver119@gmail.com>

--- libnd4j/tests_cpu/layers_tests/GraphAnalysisTests.cpp
+++ libnd4j/tests_cpu/layers_tests/GraphAnalysisTests.cpp
@@ -1109,3 +1109,25 @@ TEST_F(GraphAnalysisTests, optimizedGraph_nested_while_1) {
 
   ASSERT_EQ(NDArrayFactory::create<float>('c', {2, 2}, {13.f, 14.f, 15.f, 16.f}), results["output"]);
 }
+
+TEST_F(GraphAnalysisTests, optimizedGraph_simpleif_0_alt) {
+  auto input_0 = NDArrayFactory::create<float>('c', {2, 2}, {1.f, 2.f, 3.f, 4.f});
+  auto input_1 = NDArrayFactory::create<float>(11.f);
+
+  auto graph = Graph::fromFlatBuffers("resources/simpleif_0_alt.fb");
+
+  auto results = graph.execute({{"input_0", input_0}, {"input_1", input_1}}, {"output"});
+
+  ASSERT_EQ(NDArrayFactory::create<float>('c', {2, 2}, {3.f, 4.f, 5.f, 6.f}), results["output"]);
+}
+
+TEST_F(GraphAnalysisTests, optimizedGraph_simplewhile_1) {
+  auto input_0 = NDArrayFactory::create<float>('c', {2, 2}, {1.f, 2.f, 3.f, 4.f});
+  auto input_1 = NDArrayFactory::create<float>(21.f);
+
+  auto graph = Graph::fromFlatBuffers("resources/simplewhile_1.fb");
+
+  auto results = graph.execute({{"input_0", input_0}, {"input_1", input_1}}, {"output"});
+
+  ASSERT_EQ(NDArrayFactory::create<float>('c', {2, 2}, {24.f, 25.f, 26.f, 27.f}), results["output"]);
+}
